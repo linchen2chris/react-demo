8b3180bf42b1c8501e287658c6ec7f7225a9692e
Lin Chen <lc1990linux@gmail.com>
2020-10-22 23:19:10 +0800

    [Chen] formik-antd make life easy

--- src/02-formik-antd/FormikForm.jsx
+++ src/02-formik-antd/FormikForm.jsx
@@ -1,12 +1,17 @@
-import { Form, Field, Formik } from 'formik';
-import { Input } from 'antd';
+import { Form, Input } from "formik-antd";
+import { Formik, ErrorMessage } from "formik";
 import React from "react";
+import * as Yup from "yup";
 
 import { formData } from "../01-react-hook-form-antd/mockFormData";
-import Editable from './components/Editable';
-import OptionsConfig from './components/OptionsConfig';
-import Required from './components/Required';
-import Viewable from './components/Viewable';
+import Editable from "./components/Editable";
+import OptionsConfig from "./components/OptionsConfig";
+import Required from "./components/Required";
+import Viewable from "./components/Viewable";
+
+const schema = Yup.object().shape({
+  label: Yup.string().required(),
+});
 
 const FormikForm = () => {
   const onSubmit = (values) => {
@@ -16,14 +21,20 @@ const FormikForm = () => {
   return (
     <div>
       <h1>Friend List</h1>
-      <Formik initialValues={formData} onSubmit={onSubmit}>
+      <Formik
+        initialValues={formData}
+        onSubmit={onSubmit}
+        validationSchema={schema}
+      >
         {(props) => (
           <Form onSubmit={props.handleSubmit} onChange={props.handleSubmit}>
-            <Field as={Input} type="text" name="label" />
-            <Required name="validate.required"/>
-            <Viewable name="validate.viewable"/>
-            <Editable name="validate.editable"/>
-            <OptionsConfig name="optionsConfig.options"/>
+            <Form.Item name="label">
+              <Input type="text" name="label" />
+            </Form.Item>
+            <Required name="validate.required" />
+            <Viewable name="validate.viewable" />
+            <Editable name="validate.editable" />
+            <OptionsConfig name="optionsConfig.options" />
           </Form>
         )}
       </Formik>
